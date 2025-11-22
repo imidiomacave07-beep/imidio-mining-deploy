5277c8b758788d89b9340074e7c8214ca71f0f3b
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-11-22 18:00:09 +0200

    Update miningRoutes.js

--- miningRoutes.js
+++ miningRoutes.js
@@ -1,24 +1,20 @@
 import express from "express";
 const router = express.Router();
 
-// Status da mineração
 let miningStatus = false;
 
-// Rota para status
 router.get("/status", (req, res) => {
   res.json({ status: miningStatus ? "Rodando" : "Parado" });
 });
 
-// Rota para iniciar mineração
 router.post("/start", (req, res) => {
   miningStatus = true;
-  res.json({ message: "Mineração iniciada" });
+  res.json({ status: "Iniciada" });
 });
 
-// Rota para parar mineração
 router.post("/stop", (req, res) => {
   miningStatus = false;
-  res.json({ message: "Mineração parada" });
+  res.json({ status: "Parada" });
 });
 
 export default router;
