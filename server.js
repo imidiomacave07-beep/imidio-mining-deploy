fdaefe95ea05c4cc0959acae7bc9115daf350ac8
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-11-25 16:44:27 +0200

    git add . git commit -m "Corrigir estrutura: adicionar public e rotas de mineração"

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/api/mining", miningRoutes);
 
 app.get("/", (req, res) => {
