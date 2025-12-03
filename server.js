22a0b3131e865f9f05e1ee338bb1e6f81c2c0112
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-03 15:47:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-// Conectar ao MongoDB
+// Conectar ao MongoDB Atlas
 const mongoURI = process.env.MONGO_URI;
+
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB conectado!'))
-  .catch(err => console.error('Erro MongoDB:', err));
+  .catch(err => console.error('Erro MongoDB:', err.message));
 
 // Porta do Render
 const PORT = process.env.PORT || 10000;
 
+// Rota principal de teste
 app.get('/', (req, res) => {
   res.send('Servidor funcionando!');
 });
 
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
