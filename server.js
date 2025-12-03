04ebd8526f5928f8f73fbb9f794f016048064584
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-03 13:14:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+  console.error('Erro: variável MONGO_URI não definida!');
+  process.exit(1); // força saída para evitar tentar conectar undefined
+}
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log("✅ MongoDB conectado"))
-.catch(err => console.error("❌ Erro MongoDB:", err));
+.then(() => console.log('MongoDB conectado!'))
+.catch(err => console.error('Erro MongoDB:', err));
