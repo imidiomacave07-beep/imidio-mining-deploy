09688519b7463cfd116005deeeddef4baaf5f94f
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-03 15:12:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+import express from 'express';
 import mongoose from 'mongoose';
 
-const mongoURI = process.env.MONGO_URI;
+const app = express();
 
-if (!mongoURI) {
-  console.error('Erro: variável MONGO_URI não definida!');
-  process.exit(1); // força saída para evitar tentar conectar undefined
-}
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB conectado!'))
+  .catch(err => console.error('Erro MongoDB:', err));
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB conectado!'))
-.catch(err => console.error('Erro MongoDB:', err));
+// Porta do Render
+const PORT = process.env.PORT || 10000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
