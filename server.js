555794a0331fb31e2b7f9bf2dcd919a8b2f176a4
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-04 18:28:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+// SERVE A PASTA PUBLIC
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROTA PADRÃO
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// PORTA DO RENDER
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
