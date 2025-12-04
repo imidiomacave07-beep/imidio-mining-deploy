c9d4cd044094e6242b63670945b5efe82bf47eb0
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-04 23:52:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 10000;
 
-// Servir ficheiros estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, "public")));
 
-// Página principal
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor a correr na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
